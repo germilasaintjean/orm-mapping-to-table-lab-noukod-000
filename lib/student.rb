02c00aab05d8a61d6abf5a9a9c3cd10860c2d5ec
Saint-Jean Germila <germila@noukod.com>
2018-11-26 08:49:18 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,35 +1,12 @@
 class Student
 
-    attr_accessor :name, :grade, :db
+    attr_accessor :name, :grade
     attr_reader :id
 
-    def initialize(name:, grade:, id: nil, db:)
+    def initialize(name, grade, id=nil)
      @name = name
      @grade = grade
-     @id = some_id
+     @id = id
     end
 
-    def self.create_table
-      sql = create_table
-      (id PRIMARY KEY INTEGER,
-       name TEXT,
-       Grade STRING);
-      DB[:conn].execute(sql)
-    end
-
-    def self.drop_table
-      sql =
-     DB[:conn].execute(sql)
-    end
-
-    def self.save(name, type, db)
-        db.execute("INSERT INTO Student (name, grade) VALUES (?, ?)", name, grade)
-      end
-
-      def create
-
-      end
-
-end
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  end
