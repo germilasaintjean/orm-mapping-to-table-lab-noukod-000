a633bd86f1d90c1543cff074eb75833f6fca1471
Saint-Jean Germila <germila@noukod.com>
2018-11-26 08:00:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,14 @@
 class Student
 
+  attr_accessor :name, :id
+
+  def initialize name: id:
+    @name = name
+    @id = id
+
+  end
+
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
