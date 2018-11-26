6f20b33923562107ea9bd98016c056e989a092f8
Saint-Jean Germila <germila@noukod.com>
2018-11-26 08:25:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,15 @@
 class Student
 
-  attr_accessor :name, :grade,
-  attr_reader :id nil
+    attr_accessor :name, :grade
+    attr_reader :id nil
 
-  def initialize name: grade: id:
-    @name = name
-    @grade = grade
-    @id = some_id
+    def initialize(name:, grade:, id:)
+     @name = name
+     @grade = grade
+     @id = some_id
+    end
 
-  end
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
 
 end
+  # Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]
